243d1d7bc85c8358cb713ee8ceae52cba6c70402
fumiya-ueda <fumiya.ueda.1985@gmail.com>
2025-08-21 20:08:21 +0900

    Configure DATABASES with dj-database-url for Heroku Postgres

--- config/settings.py
+++ config/settings.py
@@ -72,17 +72,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
+import os
 
-# DB設定：本番（Heroku）は Postgres、ローカルはSQLite
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default="sqlite:///db.sqlite3",
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=True
     )
 }
+
 
 
 
